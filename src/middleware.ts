06601815bc9fd7b64f5186a80415ca2c0b7c85c6
leejinseok <sonaky47@gmail.com>
2024-09-18 22:34:06 +0900

    update - add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const url = request.url
+  const headers: HeadersInit = new Headers()
+  console.log('url', url)
+  headers.append('url', url)
+  return NextResponse.next({
+    headers
+  })
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/((?!api|_next|static|public|favicon.ico).*)',
+}
