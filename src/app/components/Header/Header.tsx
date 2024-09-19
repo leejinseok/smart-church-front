c5293b7c734e4f820fae70eb13d8a33962e395ca
leejinseok <sonaky47@gmail.com>
2024-09-19 16:12:31 +0900

    update - hek

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <nav>
+            <h2>스마트 처치</h2>
+        </nav>
+    )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import RecoilRootWrapper from "./layout/RecoilWrapper";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -24,10 +26,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
-    </html>
+    <RecoilRootWrapper>
+      <html lang="ko" className={`${geistMono.variable}`}>
+        <body>
+          {children}
+        </body>
+      </html>
+    </RecoilRootWrapper>
+
   );
 }
