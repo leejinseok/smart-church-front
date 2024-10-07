1208652e84a8598433df5192def9917b1ff1a044
leejinseok <sonaky47@gmail.com>
2024-10-07 12:47:23 +0900

    update -help

--- src/app/components/Video/Video.tsx
+++ src/app/components/Video/Video.tsx
@@ -4,6 +4,11 @@ import "./Video.scss";
 import { useEffect, useState } from "react";
 import { ChurchVideo } from "../../../api/smart-church/smart-church-api-response";
 import { loadScript } from "../../../util/script-utils";
+import { clearTimeout } from "timers";
+
+const videoOriginalWidth = 610;
+const videoOriginalHeight = 380;
+const videoAspectRatio = videoOriginalHeight / videoOriginalWidth;
 
 export default function Video({ video }: { video: ChurchVideo[] }) {
   const [mounted, setMounted] = useState(false);
@@ -19,8 +24,8 @@ export default function Video({ video }: { video: ChurchVideo[] }) {
           const loadingYoutubeIframe = (videoIndex: number) => {
             YT.ready(() => {
               new YT.Player(`church-video-${videoIndex}`, {
-                height: "380",
-                width: "610",
+                height: videoOriginalHeight,
+                width: videoOriginalWidth,
                 videoId,
                 playerVars: {},
               });
@@ -41,6 +46,35 @@ export default function Video({ video }: { video: ChurchVideo[] }) {
     };
   }, []);
 
+  useEffect(() => {
+    let timer: number | null = null;
+
+    const videoResize = () => {
+      if (timer) {
+        window.clearTimeout(timer);
+      }
+
+      const videoElements = document.querySelectorAll(
+        "#video-component ul li iframe",
+      ) as NodeListOf<HTMLElement>;
+
+      timer = window.setTimeout(() => {
+        videoElements.forEach((videoElement) => {
+          const newHeight = videoElement.clientWidth * videoAspectRatio;
+          videoElement.style.height = `${newHeight}px`;
+        });
+      }, 30);
+    };
+
+    window.addEventListener("resize", videoResize);
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      window.removeEventListener("resize", videoResize);
+    };
+  }, []);
+
   const generateVideoLengthClassName = (videoLength: number) => {
     return null;
   };
@@ -55,7 +89,10 @@ export default function Video({ video }: { video: ChurchVideo[] }) {
           return (
             <li key={itemIndex} className="col">
               <div className="d-flex justify-content-center">
-                <div id={`church-video-${itemIndex}`}></div>
+                <div
+                  id={`church-video-${itemIndex}`}
+                  className="church-vedio-container"
+                ></div>
               </div>
             </li>
           );
