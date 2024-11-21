dbb395f2eccda2b1ebb1592a044daadedf75941f
leejinseok <sonaky47@gmail.com>
2024-11-21 17:07:21 +0900

    update - Working in church info

--- src/app/components/HomepageTypeA/ChurchEdit/ChurchEditModal.tsx
+++ src/app/components/HomepageTypeA/ChurchEdit/ChurchEditModal.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { ChurchResponse } from "../../../../api/smart-church/smart-church-api-response";
 import ApplyButton from "../../../../components/common/ApplyButton";
 import { setCookie } from "../../../../util/cookie-utils";
+import { loadScript } from "../../../../util/script-utils";
 
 export default function ChurchEditModal({
   church,
@@ -20,6 +21,27 @@ export default function ChurchEditModal({
       document.body.classList.remove("overflow-hidden");
     }
   }, [visible]);
+
+  const handleAddressClick = () => {
+    const callback = () => {
+      new daum.Postcode({
+        oncomplete: function (data) {
+          // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분입니다.
+          // 예제를 참고하여 다양한 활용법을 확인해 보세요.
+
+          console.log(data);
+        },
+      }).open();
+    };
+    if (typeof daum === "undefined") {
+      loadScript(
+        "//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js",
+        callback,
+      );
+    } else {
+      callback();
+    }
+  };
 
   const handleSubmit = () => {
     setCookie(
@@ -43,18 +65,65 @@ export default function ChurchEditModal({
                 <div className="form-group">
                   <p
                     className="font-weight-bold font-size-m"
-                    style={{ marginBottom: 10 }}
+                    style={{ marginBottom: 0 }}
                   >
-                    교회이름
+                    이름
                   </p>
                   <input
                     type="text"
                     className="font-size-m no-border"
                     value={churchState.name}
+                    placeholder="교회이름을 입력해주세요"
                     onChange={(e) =>
                       setChurchState((prev) => ({
                         ...prev,
                         name: e.target.value,
+                      }))
+                    }
+                  />
+                </div>
+
+                <div className="form-group">
+                  <p
+                    className="font-weight-bold font-size-m"
+                    style={{ marginBottom: 0 }}
+                  >
+                    주소
+                  </p>
+
+                  <input
+                    type="text"
+                    className="font-size-m no-border"
+                    value={churchState.address}
+                    placeholder="교회주소를 입력해주세요"
+                    readOnly
+                    onClick={handleAddressClick}
+                    onChange={(e) =>
+                      setChurchState((prev) => ({
+                        ...prev,
+                        address: e.target.value,
+                      }))
+                    }
+                  />
+                </div>
+
+                <div className="form-group">
+                  <p
+                    className="font-weight-bold font-size-m"
+                    style={{ marginBottom: 0 }}
+                  >
+                    전화번호
+                  </p>
+
+                  <input
+                    type="text"
+                    className="font-size-m no-border width-100"
+                    value={churchState.tel || ""}
+                    placeholder="교회 전화번호를 입력해주세요"
+                    onChange={(e) =>
+                      setChurchState((prev) => ({
+                        ...prev,
+                        tel: e.target.value,
                       }))
                     }
                   />
